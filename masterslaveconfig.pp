e93d93c215b10a786217cfc1db7cca20f042e5b4
Partha Nayak <partha.pratimnayak@gmail.com>
2014-04-21 16:54:41 -0700

    Update masterslaveconfig.pp

--- masterslaveconfig.pp
+++ masterslaveconfig.pp
@@ -4,7 +4,13 @@ file {'config.sh':
      path => '/etc/puppetlabs/puppet/modules/config.sh',
      ensure => file,
      mode => 0644,
-     source => 'puppet:///files/config.sh',
+     content => ''cd /etc/postgresql/9.1/main
+mv postgresql.conf postgresql_bck_4212014.conf
+wget -O /etc/postgresql/9.1/main/postgresql.conf https://raw.githubusercontent.com/ParthNayak/Collaterals/master/postgresql.conf --no-check-certificate
+cd /etc/postgresql/9.1/main
+mv pg_hba.conf pg_hba_bck_4212014.conf
+wget -O /etc/postgresql/9.1/main/pg_hba.conf https://raw.githubusercontent.com/ParthNayak/Collaterals/master/pg_hba.conf --no-check-certificate
+sudo service postgres restart'',
      }
 exec {'exec-config':
       command => '/etc/puppetlabs/puppet/modules/config.sh',
