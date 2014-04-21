55ea9b94fa3bbbc396f405b9b44693fc97540777
Partha Nayak <partha.pratimnayak@gmail.com>
2014-04-21 14:50:26 -0700

    Update masterslaveconfig.pp

--- masterslaveconfig.pp
+++ masterslaveconfig.pp
@@ -1,13 +1,14 @@
 # This puppet class file configures the puppet instance as master or slave
 class postgresql::masterslaveconfig {
-file {'/etc/puppetlabs/puppet/modules/postgresql/manifests/config.sh':
-     ensure => file,
-     mode => 0644,
-     source => 'puppet:///etc/puppetlabs/puppet/modules/postgresql/manifests/config.sh',
+file {'config.sh':
+     path =>'/etc/puppetlabs/puppet/modules/config.sh',
+     ensure =>file,
+     mode =>0644,
+     source =>'puppet:///etc/puppetlabs/puppet/modules/postgresql/manifests/config.sh',
      }
 exec {'exec-config':
-      command =>'/etc/puppetlabs/puppet/modules/postgresql/manifests/config.sh',
-      require => File['/etc/puppetlabs/puppet/modules/postgresql/manifests/config.sh'],
+      command =>'/etc/puppetlabs/puppet/modules/config.sh',
+      require => File['/etc/puppetlabs/puppet/modules/config.sh'],
       }
       }
       
